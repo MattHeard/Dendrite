0c83c451c5b208ad67aa9e5e95a2885a09e5e4c2
Matt Heard <matt@mattheard.net>
2014-01-24 22:12:25 +1300

    Sorted the methods of PageIdTest alphabetically.

--- Dendrite/src/com/deuteriumlabs/dendrite/model/test/PageIdTest.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/model/test/PageIdTest.java
@@ -9,19 +9,6 @@ import com.deuteriumlabs.dendrite.model.PageId;
 public class PageIdTest {
 
 	@Test
-	public final void testValidNumber() {
-		final PageId id = new PageId();
-		final int validNum = 1;
-		id.setNumber(validNum);
-		
-		final int expected = validNum;
-		final int actual = id.getNumber();
-		
-		final String message = "The valid number returned was not correct.";
-		assertEquals(message, expected, actual);
-	}
-	
-	@Test
 	public final void testInvalidNumber() {
 		final PageId id = new PageId();
 		final int invalidNum = -1;
@@ -31,6 +18,31 @@ public class PageIdTest {
 		final int actual = id.getNumber();
 		
 		final String message = "The invalid number should have returned 0.";
+		assertEquals(message, expected, actual);
+	}
+	
+	@Test
+	public final void testInvalidVersion() {
+		final PageId id = new PageId();
+		final String invalidVersion = "f00";
+		id.setVersion(invalidVersion);
+		
+		final String actual = id.getVersion();
+		
+		final String message = "The valid version returned was not correct.";
+		assertNull(message, actual);
+	}
+	
+	@Test
+	public final void testValidNumber() {
+		final PageId id = new PageId();
+		final int validNum = 1;
+		id.setNumber(validNum);
+		
+		final int expected = validNum;
+		final int actual = id.getNumber();
+		
+		final String message = "The valid number returned was not correct.";
 		assertEquals(message, expected, actual);
 	}
 	
@@ -45,17 +57,5 @@ public class PageIdTest {
 		
 		final String message = "The valid version returned was not correct.";
 		assertEquals(message, expected, actual);
-	}
-	
-	@Test
-	public final void testInvalidVersion() {
-		final PageId id = new PageId();
-		final String invalidVersion = "f00";
-		id.setVersion(invalidVersion);
-		
-		final String actual = id.getVersion();
-		
-		final String message = "The valid version returned was not correct.";
-		assertNull(message, actual);
 	}
 }
