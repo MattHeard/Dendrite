b48cd3a450833e21b884ae7e0c6476265997c4e3
Matt Heard <matt@mattheard.net>
2014-01-26 12:47:58 +1300

    Created a test suite to encompass all existing `Model` unit tests.

---
+++ Dendrite/src/com/deuteriumlabs/dendrite/model/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.deuteriumlabs.dendrite.model.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PageIdTest.class, StoryOptionTest.class, StoryPageTest.class })
+public class AllTests {
+
+}
