601e2a14c564337d65384c0d36c56d960076d27a
Matt Heard <matt@mattheard.net>
2014-01-26 16:46:28 +1300

    Implemented `User.update()`.

--- Dendrite/src/com/deuteriumlabs/dendrite/model/test/UserTest.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/model/test/UserTest.java
@@ -12,23 +12,25 @@ import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestC
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 public class UserTest {
+	private static final String CHANGED_DEFAULT_PEN_NAME = "Bob Saget";
 	private static final String DUMMY_DEFAULT_PEN_NAME = "John Smith";
 	private static final String DUMMY_ID = "1234567890";
+	
 	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
 			new LocalDatastoreServiceTestConfig());
-	
+
 	private void createDummyUser() {
 		final User user = new User();
 		user.setId(DUMMY_ID);
 		user.setDefaultPenName(DUMMY_DEFAULT_PEN_NAME);
 		user.create();
 	}
-	
+
 	@Before
 	public void setUp() {
 		helper.setUp();
 	}
-	
+
 	@After
 	public void tearDown() {
 		helper.tearDown();
@@ -39,9 +41,9 @@ public class UserTest {
 		final User user = new User();
 		user.setId(DUMMY_ID);
 		user.setDefaultPenName(DUMMY_DEFAULT_PEN_NAME);
-		
+
 		user.create();
-		
+
 		final boolean isUserInStore = user.isInStore();
 		final String message = "The user was not in the store.";
 		assertTrue(message, isUserInStore);
@@ -53,10 +55,30 @@ public class UserTest {
 		final User user = new User();
 		user.setId(DUMMY_ID);
 		user.read();
-		
+
 		final String expected = DUMMY_DEFAULT_PEN_NAME;
 		final String actual = user.getDefaultPenName();
 		final String message = "The default pen name was not correct.";
+		assertEquals(message, expected, actual);
+	}
+
+	@Test
+	public final void testUpdate() {
+		createDummyUser();
+
+		final User userBeforeUpdate = new User();
+		userBeforeUpdate.setId(DUMMY_ID);
+		userBeforeUpdate.read();
+
+		userBeforeUpdate.setDefaultPenName(CHANGED_DEFAULT_PEN_NAME);
+		userBeforeUpdate.update();
+
+		final User userAfterUpdate = new User();
+		userAfterUpdate.setId(DUMMY_ID);
+		userAfterUpdate.read();
+		final String expected = CHANGED_DEFAULT_PEN_NAME;
+		final String actual = userAfterUpdate.getDefaultPenName();
+		final String message = "The changed default pen name was not correct.";
 		assertEquals(message, expected, actual);
 	}
 
