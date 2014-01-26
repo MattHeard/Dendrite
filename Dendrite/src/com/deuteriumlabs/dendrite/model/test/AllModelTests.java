03e0c158090bc2269328f6a5e99e9bd3cb0a5c85
Matt Heard <matt@mattheard.net>
2014-01-26 15:54:16 +1300

    Renamed `AllTests` to `AllModelTests`.
    I am making another test suite called `AllViewTests`, so I need a more
    distinct name for the test suite covering `Model` and its subclasses.

--- Dendrite/src/com/deuteriumlabs/dendrite/model/test/AllModelTests.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/model/test/AllModelTests.java
@@ -7,6 +7,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ PageIdTest.class, StoryBeginningTest.class,
 		StoryOptionTest.class, StoryPageTest.class })
-public class AllTests {
+public class AllModelTests {
 
 }
