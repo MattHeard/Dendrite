a0a22ab668151426ea6cd16bd02a4e8e1a0c1d27
Matt Heard <matt@mattheard.net>
2014-05-29 20:18:56 +1200

    Removed unused tests.

--- Dendrite/src/com/deuteriumlabs/dendrite/view/test/ContentsViewTest.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/view/test/ContentsViewTest.java
@@ -6,18 +6,6 @@ import org.junit.Test;
 import com.deuteriumlabs.dendrite.view.ContentsView;
 
 public class ContentsViewTest {
-
-	@Test
-	public final void testIsUserLoggedIn() {
-		// I currently do not know how to test the Google App Engine user with a
-		// unit test.
-	}
-	
-	@Test
-	public final void testGetLoginLink() {
-		// I currently do not know how to test the Google App Engine user
-		// service with a unit test.
-	}
 	
 	@Test
 	public final void testGetWebPageTitle() {
