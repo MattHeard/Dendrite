141b3cccd700b2aa62326a60fb78d6ad153e8dcf
Matt Heard <matt@mattheard.net>
2014-05-29 20:02:33 +1200

    Added unit test for `getWebPageTitle()`.

--- Dendrite/src/com/deuteriumlabs/dendrite/view/test/ContentsViewTest.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/view/test/ContentsViewTest.java
@@ -1,6 +1,9 @@
 package com.deuteriumlabs.dendrite.view.test;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
+import com.deuteriumlabs.dendrite.view.ContentsView;
 
 public class ContentsViewTest {
 
@@ -14,5 +17,14 @@ public class ContentsViewTest {
 	public final void testGetLoginLink() {
 		// I currently do not know how to test the Google App Engine user
 		// service with a unit test.
+	}
+	
+	@Test
+	public final void testGetWebPageTitle() {
+		final String message = "The web page title is incorrect.";
+		final String expected = "Dendrite";
+		final ContentsView view = new ContentsView();
+		final String actual = view.getWebPageTitle();
+		assertEquals(message, expected, actual);
 	}
 }
