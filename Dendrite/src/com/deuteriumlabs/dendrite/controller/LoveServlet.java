8d9f5be36aa92477dea9c492f3a7fb4b9cf7e3f6
Matt Heard <matt@mattheard.net>
2014-09-16 20:55:05 +1200

    Removed `user` parameter from `/love` request.
    Partially resolves issue #184.

--- Dendrite/src/com/deuteriumlabs/dendrite/controller/LoveServlet.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/controller/LoveServlet.java
@@ -10,107 +10,98 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.deuteriumlabs.dendrite.model.PageId;
+import com.deuteriumlabs.dendrite.model.User;
 
 /**
  * 
  */
 public class LoveServlet extends DendriteServlet {
 
-    /**
+	/**
      * 
      */
-    private static final long serialVersionUID = 7662290110231192243L;
-    private static final String PAGE_ID_PARAM_NAME = "p";
-    private static final String USER_ID_PARAM_NAME = "user";
-    private static final String IS_ADDED_PARAM_NAME = "isAdded";
-    
-    private int count;
+	private static final long serialVersionUID = 7662290110231192243L;
+	private static final String PAGE_ID_PARAM_NAME = "p";
+	private static final String IS_ADDED_PARAM_NAME = "isAdded";
 
-    @Override
-    protected void doPost(final HttpServletRequest req,
-            final HttpServletResponse resp) throws ServletException,
-            IOException {
-        final LoveController controller = new LoveController();
-        
-        final PageId pageId = this.extractPageId(req);
-        final String userId = this.extractUserId(req);
-        if (!pageId.isValid()) {
-            this.setInvalidPageIdError(resp);
-        } else if (userId == null) {
-            this.setInvalidUserIdError(resp);
-        } else {
-            controller.pageId = pageId;
-            controller.loverId = userId;
-            
-            final boolean isLoveAdded = this.extractIsLoveAdded(req);
-            final int count;
-            if (isLoveAdded) {
-                count = controller.addLove();
-            } else {
-                count = controller.removeLove();
-            }
-            this.count = count;
-            this.setLoveOkResponse(resp);
-        }
-    }
+	private int count;
 
-    /**
-     * @param resp
-     * @throws IOException 
-     */
-    private void setLoveOkResponse(final HttpServletResponse resp)
-            throws IOException {
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().println(count);
-    }
+	@Override
+	protected void doPost(final HttpServletRequest req,
+			final HttpServletResponse resp) throws ServletException,
+			IOException {
+		final LoveController controller = new LoveController();
 
-    /**
-     * @param resp 
-     * @throws IOException 
-     * 
-     */
-    private void setInvalidPageIdError(final HttpServletResponse resp)
-            throws IOException {
-        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        resp.getWriter().println("PageId is invalid.");
-    }
-    
-    /**
-     * @param resp 
-     * @throws IOException 
-     * 
-     */
-    private void setInvalidUserIdError(final HttpServletResponse resp)
-            throws IOException {
-        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        resp.getWriter().println("UserId is invalid.");
-    }
+		final PageId pageId = this.extractPageId(req);
+		final String userId = User.getMyUserId();
+		if (!pageId.isValid()) {
+			this.setInvalidPageIdError(resp);
+		} else if (userId == null) {
+			this.setInvalidUserIdError(resp);
+		} else {
+			controller.pageId = pageId;
+			controller.loverId = userId;
 
-    /**
-     * @param req
-     * @return
-     */
-    private boolean extractIsLoveAdded(final HttpServletRequest req) {
-        final String paramVal = req.getParameter(IS_ADDED_PARAM_NAME);
-        return Boolean.valueOf(paramVal);
-    }
+			final boolean isLoveAdded = this.extractIsLoveAdded(req);
+			final int count;
+			if (isLoveAdded) {
+				count = controller.addLove();
+			} else {
+				count = controller.removeLove();
+			}
+			this.count = count;
+			this.setLoveOkResponse(resp);
+		}
+	}
 
-    /**
-     * @param req
-     * @return
-     */
-    private String extractUserId(final HttpServletRequest req) {
-        return req.getParameter(USER_ID_PARAM_NAME);
-    }
+	/**
+	 * @param resp
+	 * @throws IOException
+	 */
+	private void setLoveOkResponse(final HttpServletResponse resp)
+			throws IOException {
+		resp.setStatus(HttpServletResponse.SC_OK);
+		resp.getWriter().println(count);
+	}
 
-    /**
-     * @param req
-     * @return
-     */
-    private PageId extractPageId(final HttpServletRequest req) {
-        final String paramVal = req.getParameter(PAGE_ID_PARAM_NAME);
-        return new PageId(paramVal);
-    }
+	/**
+	 * @param resp
+	 * @throws IOException
+	 * 
+	 */
+	private void setInvalidPageIdError(final HttpServletResponse resp)
+			throws IOException {
+		resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		resp.getWriter().println("PageId is invalid.");
+	}
 
-    
+	/**
+	 * @param resp
+	 * @throws IOException
+	 * 
+	 */
+	private void setInvalidUserIdError(final HttpServletResponse resp)
+			throws IOException {
+		resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		resp.getWriter().println("UserId is invalid.");
+	}
+
+	/**
+	 * @param req
+	 * @return
+	 */
+	private boolean extractIsLoveAdded(final HttpServletRequest req) {
+		final String paramVal = req.getParameter(IS_ADDED_PARAM_NAME);
+		return Boolean.valueOf(paramVal);
+	}
+
+	/**
+	 * @param req
+	 * @return
+	 */
+	private PageId extractPageId(final HttpServletRequest req) {
+		final String paramVal = req.getParameter(PAGE_ID_PARAM_NAME);
+		return new PageId(paramVal);
+	}
+
 }
