afd4268b5e846a02f6f73e3791a6b6603211a3c1
Matt <matt@mattheard.net>
2015-05-12 18:41:36 +1200

    Dealt with Russian spam bots.
    A few days ago, bots started spamming Dendrite by rewriting hundreds of
    pages and starting hundreds of new stories. Dendrite usually stays
    within the free quota of Google App Engine, so when the bots started
    making loads of requests to the datastore, I started getting charged.
    I turned off the billing and Dendrite went down for a day. I added
    `dos.xml` to block the subnets which the bot requests originated from.
    Once I confirmed that the bots were getting 403 FORBIDDEN responses to
    their HTTP requests, I built a tool called `BotCleaner` to erase large
    numbers of alternative versions for pages that the bots had written. It
    worked in my test environment but not on the live, public system. This
    meant that I had to spend an hour or so manually deleting large sets of
    pages and stories using the Google Cloud console in my browser.
    
    A tool should be written to flag suspicious submissions and prep them in
    batches for mass deletion once verified by a human as being all spam.

---
+++ Dendrite/src/com/deuteriumlabs/dendrite/utilities/BotCleaner.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.deuteriumlabs.dendrite.utilities;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.deuteriumlabs.dendrite.model.PageId;
+import com.deuteriumlabs.dendrite.model.StoryPage;
+
+/**
+ * 
+ */
+public class BotCleaner extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -8203238739960697515L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("BOT CLEANER TOOL");
+        resp.getWriter().println();
+        final String numParam = req.getParameter("num");
+        int pgNum = 0;
+        try {
+        	pgNum = Integer.parseInt(numParam);
+        } catch (NumberFormatException e) {
+        	resp.getWriter().println("ERROR: Could not get valid page number");
+        	return;
+        }
+        final PageId id = new PageId();
+        id.setNumber(pgNum);
+        List<StoryPage> pgs = StoryPage.getAllVersions(id);
+        int count = 0;
+        for (StoryPage pg : pgs) {
+        	pg.delete();
+        	count++;
+        }
+        resp.getWriter().println("Deleted " + count + " versions.");
+        
+    }
+}
