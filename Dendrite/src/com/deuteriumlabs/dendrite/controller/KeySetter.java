5514a5484b6c394cfd3edbe265deb4dd1086e087
MattHeard <matt@mattheard.net>
2015-12-29 21:06:52 +1300

    Finish setting StoryPage key from ID

--- Dendrite/src/com/deuteriumlabs/dendrite/controller/KeySetter.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/controller/KeySetter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.Cursor;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -40,34 +41,48 @@ public class KeySetter extends DendriteServlet {
         response.setContentType("text/plain");
 	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 	    final int limit = 10;
-        FetchOptions fetchOptions = FetchOptions.Builder.withLimit(limit);
-        final Query query = new Query("StoryPage");
+        Cursor startCursor;
+        FetchOptions fetchOptions;
+        fetchOptions = FetchOptions.Builder.withLimit(limit);
+        final Query query = new Query("StoryPage").addSort(Entity.KEY_RESERVED_PROPERTY);
         final PreparedQuery preparedQuery = datastore.prepare(query);
-        final QueryResultList<Entity> results = preparedQuery.asQueryResultList(fetchOptions);
         final TransactionOptions options = TransactionOptions.Builder.withXG(true);
-        final Transaction transaction = datastore.beginTransaction(options);
-        try {
-            for (final Entity oldEntity : results) {
-                try {
-                    response.getWriter().println("old: " + oldEntity);
-                    String id = oldEntity.getProperty("idNumber").toString();
-                    id += oldEntity.getProperty("idVersion").toString();
-                    Entity newEntity = new Entity("StoryPage", id);
-                    newEntity.setPropertiesFrom(oldEntity);
-                    response.getWriter().println("new: " + newEntity);
-                    response.getWriter().println();
-                    
-                    datastore.delete(oldEntity.getKey());
-                    datastore.put(newEntity);
-                } catch (IOException e) {
-                    // IGNORE IOException
+        QueryResultList<Entity> results = preparedQuery.asQueryResultList(fetchOptions);
+        int count = 0;
+        while (results.size() > 0 && count < 1000) {
+            final Transaction transaction = datastore.beginTransaction(options);
+            try {
+                for (final Entity oldEntity : results) {
+                    try {
+                        count++;
+                        response.getWriter().println("#" + count);
+                        response.getWriter().println("old: " + oldEntity);
+                        if (oldEntity.hasProperty("idNumber") &&
+                                oldEntity.hasProperty("idVersion")) {
+                            String id = oldEntity.getProperty("idNumber").toString();
+                            id += oldEntity.getProperty("idVersion").toString();
+                            Entity newEntity = new Entity("StoryPage", id);
+                            newEntity.setPropertiesFrom(oldEntity);
+                            response.getWriter().println("new: " + newEntity);
+                            response.getWriter().println();
+                            datastore.delete(oldEntity.getKey());
+                            datastore.put(newEntity);
+                        } else {
+                            response.getWriter().println("ERROR: Invalid Page ID");
+                        }
+                    } catch (IOException e) {
+                        // IGNORE IOException
+                    }
+                }
+                transaction.commit();
+            } finally {
+                if (transaction.isActive()) {
+                    transaction.rollback();
                 }
             }
-            transaction.commit();
-        } finally {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
+            startCursor = results.getCursor();
+            fetchOptions = FetchOptions.Builder.withLimit(limit).startCursor(startCursor);
+            results = preparedQuery.asQueryResultList(fetchOptions);
         }
 	}
 }
