6d6b772af5b7073f3fb52e3fcdf230530bf2b701
Matt Heard <matt@mattheard.net>
2016-04-17 11:37:53 +1200

    Assert exception thrown querying blank story page

--- Dendrite/src/com/deuteriumlabs/dendrite/unittest/StoryPageTest.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/unittest/StoryPageTest.java
@@ -2,14 +2,26 @@ package com.deuteriumlabs.dendrite.unittest;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.deuteriumlabs.dendrite.model.StoryPage;
 
 public class StoryPageTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @Test
     public void testBlankStoryPageHasNoBeginning() {
         final StoryPage storyPage = new StoryPage();
         assertEquals(storyPage.getBeginning(), null);
+    }
+
+    @Test
+    public void testExceptionDeterminingWhetherBlankPageIsFirst() {
+        final StoryPage storyPage = new StoryPage();
+        exception.expect(NullPointerException.class);
+        storyPage.determineWhetherFirstPg();
     }
 }
