3af6ec2f9cf9bf41d738fd4999283c6d368712ff
Matt Heard <matt@mattheard.net>
2016-04-20 16:07:50 +1200

    Add test for default constructor

--- Dendrite/src/com/deuteriumlabs/dendrite/unittest/PageIdTest.java
+++ Dendrite/src/com/deuteriumlabs/dendrite/unittest/PageIdTest.java
@@ -1,12 +1,13 @@
 package com.deuteriumlabs.dendrite.unittest;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import com.deuteriumlabs.dendrite.model.PageId;
 
 public class PageIdTest {
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testDefaultConstructor() {
+        final PageId pageId = new PageId();
+        assertNotNull(pageId);
     }
 }
