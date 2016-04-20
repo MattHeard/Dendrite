f7caab2777cf99bc5648fc5011c77f39e6d15cec
Matt Heard <matt@mattheard.net>
2016-04-20 16:05:45 +1200

    Add empty test suite

---
+++ Dendrite/src/com/deuteriumlabs/dendrite/unittest/PageIdTest.java
@@ -0,0 +1,12 @@
+package com.deuteriumlabs.dendrite.unittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PageIdTest {
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
